8edb9fac7036e15df2859d00ac7bf5e8761ebc47
Ben Helleman <bhellema@adobe.com>
2025-07-31 14:27:03 -0400

    library metadata

--- blocks/library-metadata/library-metadata.js
+++ blocks/library-metadata/library-metadata.js
@@ -14,7 +14,7 @@ export default async function decorate(block) {
   newBlock.append(heading);
   newBlock.append(descEl);
 
-  // moveInstrumentation(descriptionElement, descriptionBlock);
+  moveInstrumentation(block, newBlock);
 
   newBlock.classList.add('library-metadata');
   block.replaceChildren(newBlock);
