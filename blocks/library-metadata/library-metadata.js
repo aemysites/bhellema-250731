3f2920cadc994db33e0b010613505bfd4e89b3a2
Ben Helleman <bhellema@adobe.com>
2025-07-31 14:30:05 -0400

    library metadata

--- blocks/library-metadata/library-metadata.js
+++ blocks/library-metadata/library-metadata.js
@@ -2,20 +2,20 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
   // Look for the block name in the first div's second child
-  const blockNameElement = block.querySelector('div:first-child > div:nth-child(2)');
-  const blockname = blockNameElement?.textContent?.trim() || 'Block Name';
+  // const blockNameElement = block.querySelector('div:first-child > div:nth-child(2)');
+  // const blockname = blockNameElement?.textContent?.trim() || 'Block Name';
 
-  // Look for the description in the second div's second child
-  const descEl = block.querySelector('div:nth-child(2) > div:nth-child(2)');
-  const newBlock = document.createElement('div');
+  // // Look for the description in the second div's second child
+  // const descEl = block.querySelector('div:nth-child(2) > div:nth-child(2)');
+  // const newBlock = document.createElement('div');
 
-  const heading = document.createElement('h2');
-  heading.textContent = blockname;
-  newBlock.append(heading);
-  newBlock.append(descEl);
+  // const heading = document.createElement('h2');
+  // heading.textContent = blockname;
+  // newBlock.append(heading);
+  // newBlock.append(descEl);
 
-  moveInstrumentation(block, newBlock);
+  // moveInstrumentation(block, newBlock);
 
-  newBlock.classList.add('library-metadata');
-  block.replaceChildren(newBlock);
+  // newBlock.classList.add('library-metadata');
+  // block.replaceChildren(newBlock);
 }
