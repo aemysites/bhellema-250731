aa50f0dcc74f5d02fb4518d7b96697d03950ee79
Ben Helleman <bhellema@adobe.com>
2025-07-31 14:15:58 -0400

    library metadata

--- blocks/library-metadata/library-metadata.js
+++ blocks/library-metadata/library-metadata.js
@@ -1,3 +1,5 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default async function decorate(block) {
   // Look for the block name in the first div's second child
   const blockNameElement = block.querySelector('div:first-child > div:nth-child(2)');
@@ -8,6 +10,7 @@ export default async function decorate(block) {
   const description = descriptionElement || document.createElement('div');
 
   const newBlock = document.createElement('div');
+  moveInstrumentation(block, newBlock);
   newBlock.innerHTML = `<h2>${blockname}</h2>${description.outerHTML}`;
   newBlock.classList.add('library-metadata');
   block.replaceChildren(newBlock);
