e6a8c140405117367c90a78d3673867904ced396
Ben Helleman <bhellema@adobe.com>
2025-08-21 14:27:06 -0400

    updates for tab block

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -14,7 +14,7 @@ export default async function decorate(block) {
     .map((child) => child.firstElementChild);
 
   tabHeadings.forEach((tab, i) => {
-    const id = toClassName(tab.textContent);
+    const id = toClassName(`${tab.textContent}-${i}`);
 
     // decorate tabpanel
     const tabpanel = block.children[i];
