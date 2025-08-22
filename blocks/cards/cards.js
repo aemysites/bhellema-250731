c6331c949590a14c3987189c1288488484f87cdb
Ben Helleman <bhellema@adobe.com>
2025-08-22 15:47:01 -0400

    Add new parsers

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -8,17 +8,15 @@ export default function decorate(block) {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div, index) => {
-      if (index === 0) {
-        // First column: style - apply the text content as a class name
-        // first column contains name of the block, and style of the block
-        // eg, "card, dark"
+    [...li.children].forEach((div, index, children) => {
+      // if we have 3 cells, then we have styles
+      if (children.length === 3 && index === 0) {
         const styleClass = div.textContent?.trim().toLowerCase().replace(',', '');
         if (styleClass) {
           li.className = styleClass;
         }
         div.remove(); // Remove the style column from the DOM
-      } else if (index === 1 && div.children.length === 1 && div.querySelector('picture')) {
+      } else if (div.querySelector('picture')) {
         // Second column: image
         div.className = 'cards-card-image';
       } else {
