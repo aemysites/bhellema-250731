0b85bb8af79a88c5cc013be1f9e08995b20cb2a1
Ben Helleman <bhellema@adobe.com>
2025-10-01 10:01:04 -0400

    lib

--- blocks/library-metadata/library-metadata.js
+++ blocks/library-metadata/library-metadata.js
@@ -1,12 +1,15 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default async function decorate(block) {
   [...block.children].forEach((child, index) => {
     const directDivs = [...child.children].filter((el) => el.tagName === 'DIV');
-
     if (directDivs.length >= 2) {
       directDivs[0].style.display = 'none';
       if (index === 0) {
         const secondDiv = directDivs[1];
         const h2 = document.createElement('h2');
+        moveInstrumentation(secondDiv, h2);
+
         h2.innerHTML = secondDiv.innerHTML;
         child.replaceChild(h2, secondDiv);
       }
